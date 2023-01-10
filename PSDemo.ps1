d95476dbf41b8f9d8838567d3aabb36dbca0e1ad
Mesrine88 <vitalka@vivaldi.net>
2023-01-10 08:06:40 +0100

    added lines

--- PSDemo.ps1
+++ PSDemo.ps1
@@ -2,3 +2,9 @@ Get-ComputerInfo
 
 PS C:\GitHub\PS_Demo> git add .
 PS C:\GitHub\PS_Demo> git commit -m "added line"
+
+# In den richtigen Ordner wechseln falls notwendig
+cd .\PS_Demo\
+
+# Die neuste Version holen
+git pull
